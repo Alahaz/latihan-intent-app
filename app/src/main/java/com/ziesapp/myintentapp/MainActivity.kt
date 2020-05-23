17f8d63db4ae5f4c2dbed7c6a3ef2417ab3cc30e
Alief Azies <Alief.ahmadazies@gmail.com>
2020-05-23 17:23:36 +0700

    01 add android studio file

---
+++ app/src/main/java/com/ziesapp/myintentapp/MainActivity.kt
@@ -0,0 +1,24 @@
+ package com.ziesapp.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+ class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnPindahAct:Button = findViewById(R.id.btnPindahAct)
+        btnPindahAct.setOnClickListener(this)
+    }
+
+     override fun onClick(v: View?) {
+         when(v.id){
+             R.id.btnPindahAct ->{
+             }
+         }
+     }
+ }
